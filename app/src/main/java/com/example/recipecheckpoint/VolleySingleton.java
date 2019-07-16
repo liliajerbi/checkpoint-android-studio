a9bcfff778e38adf65e6277d7af9ef442f513218
Lilia Jerbi <lilia-jerbi@hotmail.fr>
2019-07-16 22:13:28 +0200

    post and get recipes in volley added

--- app/src/main/java/com/example/recipecheckpoint/VolleySingleton.java
+++ app/src/main/java/com/example/recipecheckpoint/VolleySingleton.java
@@ -10,16 +10,22 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.recipecheckpoint.model.Recipe;
 import com.example.recipecheckpoint.model.User;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class VolleySingleton {
@@ -99,5 +105,125 @@ public class VolleySingleton {
                 }
             }
         };
-        addToRequestQueue(jsonObjectRequest);    }
+        addToRequestQueue(jsonObjectRequest);
+    }
+
+    public void signIn(User user, final Consumer<User> listener) {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        final Gson gson = gsonBuilder.create();
+        final String requestBody = gson.toJson(user);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST,
+                SIGN_IN_URL, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                User user = gson.fromJson(response.toString(), User.class);
+                listener.accept(user);
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                new AlertDialog.Builder(context)
+                        .setTitle("Error")
+                        .setMessage(error.getMessage())
+                        .show();
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+
+            @Override
+            public byte[] getBody() {
+                try {
+                    return requestBody == null ? null : requestBody.getBytes("utf-8");
+                } catch (UnsupportedEncodingException uee) {
+                    VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s",
+                            requestBody, "utf-8");
+                    return null;
+                }
+            }
+        };
+
+        addToRequestQueue(jsonObjectRequest);
+    }
+
+    public void getAllRecipes(final Consumer<List<Recipe>> collectPointListener) {
+        String url =  API_URL + "recipes";
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        final Gson gson = gsonBuilder.create();
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        List<Recipe> recipes = new ArrayList<>();
+                        if (response.length() > 0) {
+                            recipes = Arrays.asList(gson.fromJson(response.toString(), Recipe[].class));
+                        }
+                        collectPointListener.accept(recipes);
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.e("Error: ", error.getMessage());
+            }
+        });
+        addToRequestQueue(jsonArrayRequest);
+    }
+
+
+    public void postRecipe(Recipe recipe, User user, final Consumer<Recipe> listener) {
+
+        String url = API_URL + "user" + user.getId() + "recipes";
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        final Gson gson = gsonBuilder.create();
+        final String requestBody = gson.toJson(recipe);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST,
+                url, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                Recipe recipe = gson.fromJson(response.toString(), Recipe.class);
+                listener.accept(recipe);
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                new AlertDialog.Builder(context)
+                        .setTitle("Error")
+                        .setMessage(error.getMessage())
+                        .show();
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+
+            @Override
+            public byte[] getBody() {
+                try {
+                    return requestBody == null ? null : requestBody.getBytes("utf-8");
+                } catch (UnsupportedEncodingException uee) {
+                    VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s",
+                            requestBody, "utf-8");
+                    return null;
+                }
+            }
+        };
+        addToRequestQueue(jsonObjectRequest);
+    }
 }
