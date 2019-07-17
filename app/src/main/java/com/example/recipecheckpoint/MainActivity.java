b844a22712f540b513248b73276fcfcb0394fd9d
Lilia Jerbi <lilia-jerbi@hotmail.fr>
2019-07-17 13:39:54 +0200

    sign in done

--- app/src/main/java/com/example/recipecheckpoint/MainActivity.java
+++ app/src/main/java/com/example/recipecheckpoint/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.recipecheckpoint;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.util.Consumer;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.recipecheckpoint.model.User;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,8 +22,28 @@ public class MainActivity extends AppCompatActivity {
         btSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent goToHome = new Intent(MainActivity.this, HomeActivity.class);
-                startActivity(goToHome);
+                EditText etEmail = findViewById(R.id.etEmailSignIn);
+                EditText etPassword = findViewById(R.id.etPasswordSignIn);
+                String email = etEmail.getText().toString();
+                String password = etPassword.getText().toString();
+                if (email.isEmpty() || password.isEmpty()) {
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setMessage("Tous les champs sont requis")
+                            .show();
+                }
+
+                User user = new User();
+                user.setEmail(email);
+                user.setPassword(password);
+
+                VolleySingleton.getInstance(MainActivity.this).signIn(user, new Consumer<User>() {
+                    @Override
+                    public void accept(User user) {
+                        UserSingleton.getInstance().setUser(user);
+                        Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                        startActivity(intent);
+                    }
+                });
             }
         });
 
